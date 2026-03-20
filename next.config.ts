260a929dd0a0be85b0df825271367ae5c8c99cac
Gabriel Laroche <144166325+gablaroche29@users.noreply.github.com>
2026-03-20 13:56:30 -0400

    feat: add action to build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['three'],
+    transpilePackages: ['three'],
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
